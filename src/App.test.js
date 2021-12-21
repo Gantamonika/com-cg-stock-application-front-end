389a0eeb52186b51eb3de3272cfd7b61b303f709
Monika <gantamonika1998@gmail.com>
2021-12-21 13:12:29 +0530

    FInal test cases push

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,24 @@ import StockData from './components/StockData';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import UpdateStock from './components/UpdateStock';
+import ManagerData from './components/ManagerData';
+import UpdateManager from './components/UpdateManager';
+import AdminData from './components/AdminData';
+import UpdateAdmin from './components/UpdateAdmin';
+import AddAdmin from './components/AddAdmin';
+import InvestorData from './components/InvestorData';
+import UpdateInvestor from './components/UpdateInvestor';
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -137,4 +155,221 @@ test('render Data Find Stock by id negative test case ', () => {
   expect(linkElement).not.toBe('Some other text which is not present in the component.');
 });
 
-//
+//Manager---------------------------------
+
+// positive test case 
+test('render Data from managerData', () => {
+  render(
+    <Provider store={store} >
+      <ManagerData/>
+    </Provider>);
+  const linkElement = screen.getByText('Manager');
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+// negative test case 
+test('render Data from managerData', () => {
+  render(
+    <Provider store={store} >
+      <ManagerData />
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+
+test('render Data from Update manager', () => {
+   
+  render(
+    <Provider store={store} >
+       <UpdateManager/>
+    </Provider>)
+  const linkElement = screen.getByText('Update manager');
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('render Data from Update manager', () => {
+  render(
+    <Provider store={store} >
+      <UpdateManager/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+test('render Data from List of all Manager', () => {
+  render(
+    <Provider store={store} >
+       <ManagerData/>
+    </Provider>)
+  const linkElement = screen.getByText('Find all manager');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data List of all Manager', () => {
+  render(
+    <Provider store={store} >
+      <ManagerData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+test('render Data from Find Manager by id', () => {
+   
+  render(
+    <Provider store={store} >
+       <ManagerData/>
+    </Provider>)
+  const linkElement = screen.getByText('Find Manager By Id');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data Find manager by id', () => {
+  render(
+    <Provider store={store} >
+      <ManagerData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+//Admin--------------------------------------------------
+
+
+
+
+
+
+
+test('render Data from AdminData', () => {
+  render(
+    <Provider store={store} >
+      <AdminData />
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+test('render Data from Update Admin', () => {
+  render(
+    <Provider store={store} >
+      <UpdateAdmin/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+test('render Data Delete Admin by id', () => {
+  render(
+    <Provider store={store} >
+      <AdminData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+test('render Data from Add New Admin', () => {
+  render(
+    <Provider store={store} >
+      <AddAdmin/>
+    </Provider>)
+  const linkElement = screen.getByText('Add Admin');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data Add New Admin', () => {
+  render(
+    <Provider store={store} >
+      <AddAdmin/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+
+test('render Data List of all Admin', () => {
+  render(
+    <Provider store={store} >
+      <AdminData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+
+test('render Data Find Admin by id', () => {
+  render(
+    <Provider store={store} >
+      <AdminData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+})
+
+//Investor--------------------------------------------------
+
+// positive test case 
+test('render Data from InvestorData', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData />
+    </Provider>);
+  const linkElement = screen.getByText('Investor');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data Delete Investor by id', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+test('render Data from Add New Investor', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData/>
+    </Provider>)
+  const linkElement = screen.getByText('Add Investor');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('render Data Add New Investor', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+test('render Data List of all Investors', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
+
+
+test('render Data Find Investor by id', () => {
+  render(
+    <Provider store={store} >
+      <InvestorData/>
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
